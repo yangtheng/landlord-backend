94b811690f80eb9dc0874dea35ff7788aa981278
yangtheng <yangtheng92@gmail.com>
2020-01-30 02:40:49 +0800

    commit jan 29

--- index.js
+++ index.js
@@ -4,33 +4,52 @@ const socketio = require('socket.io');
 const cors = require('cors');
 const uuid = require('uuid/v1');
 
-const { createRoom } = require('./rooms');
+const { createRoom, joinRoom, leaveRoom, disconnect, getRoom, getRooms } = require('./rooms');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connect', (socket) => {
-
+  console.log(`${socket.id} connected`);
   socket.on('createRoom', user => {
-    const roomId = uuid()
+    const roomId = uuid();
+    socket.join(roomId);
     createRoom(roomId, socket.id, user);
     console.log(`room ${roomId} created`);
-    // io.emit()
+    io.emit('reduxActionReceived', {
+      type: 'roomInfo/REC_CREATE_ROOM',
+      rooms: getRooms(),
+    });
   })
 
   socket.on('joinRoom', ({ user, roomId }) => {
     socket.join(roomId);
+    joinRoom(roomId, socket.id, user)
     console.log(`${socket.id} connected to ${roomId}`);
+    io.to(roomId).emit('reduxActionReceived', {
+      type: 'roomInfo/REC_USER_JOIN',
+      users: getRoom().users,
+    });
   });
 
   socket.on('leaveRoom', ({ user, roomId }) => {
     socket.leave(roomId);
+    leaveRoom(roomId, socket.id)
     console.log(`${socket.id} left ${roomId}`);
+    socket.broadcast.to(roomId).emit('reduxActionReceived', {
+      type: 'roomInfo/REC_USER_LEAVE',
+      users: getRoom().users,
+    });
   });
 
   socket.on('disconnect', () => {
+    const roomId = disconnect(socket.id)
     console.log(`${socket.id} disconnected`);
+    socket.broadcast.to(roomId).emit('reduxActionReceived', {
+      type: 'roomInfo/REC_USER_LEAVE',
+      users: getRoom().users,
+    });
   })
 
   socket.on('reduxActionSent', (action) => {
--- rooms.js
+++ rooms.js
@@ -8,6 +8,7 @@ const createRoom = (roomId, socketId, user) => {
       user,
     }]
   };
+
   rooms = [
     ...rooms,
     roomObject,
@@ -16,4 +17,62 @@ const createRoom = (roomId, socketId, user) => {
   console.log(rooms);
 };
 
-module.exports = { createRoom };
+const joinRoom = (roomId, socketId, user) => {
+  rooms = rooms.map(room => {
+    if (room.roomId === roomId) {
+      if (room.users.length >= 3) return room
+      return {
+        ...room,
+        ...{
+          users: [
+            ...room.users,
+            {
+              socketId,
+              user,
+            },
+          ],
+        },
+      };
+    } else {
+      return room;
+    }
+  });
+};
+
+const leaveRoom = (roomId, socketId) => {
+  rooms = rooms.map(room => {
+    if (room.roomId === roomId) {
+      return {
+        ...room,
+        ...{
+          users: room.users.filter(roomUser => socketId !== roomUser.socketId),
+        },
+      };
+    } else {
+      return room;
+    }
+  });
+};
+
+const disconnect = socketId => {
+  let roomId;
+  rooms = rooms.map(room => {
+    if (room.users.indexOf(user => user.socketId === socketId) === -1) {
+      return room;
+    }
+    roomId = room.roomId;
+    return {
+      ...room,
+      ...{
+        users: room.users.filter(roomUser => socketId !== roomUser.socketId),
+      },
+    };
+  });
+  return roomId;
+};
+
+const getRoom = roomId => rooms.filter(room => room.roomId === roomId)[0] || [];
+
+const getRooms = () => rooms;
+
+module.exports = { createRoom, joinRoom, leaveRoom, disconnect, getRoom, getRooms };
